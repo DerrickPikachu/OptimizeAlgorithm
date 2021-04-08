7ce27a8e5e1255b7892ed8a09db31fde08b7657f
pikaChin <derrfgh4563@gmail.com>
2021-04-08 14:41:26 +0800

    Parameter Test

--- GA_algorithm/GAtrain.m
+++ GA_algorithm/GAtrain.m
@@ -7,7 +7,7 @@ lby = -3;
 % Setup some parameters of GA
 survived = 0.80;
 crossover = 0.90;
-mutation = 0.01;
+mutation = 0.02;
 
 myGA = GA(10, @targetFunction, ubx, lbx, uby, lby);
 myGA = myGA.setParameters(survived, crossover, mutation);
