a05a5af715523ab1e5cead09d2a4fa8344efd19b
pikaChin <derrfgh4563@gmail.com>
2021-04-29 11:37:07 +0800

    lion crossover function

--- Lion_algorithm/PrideOfLion.m
+++ Lion_algorithm/PrideOfLion.m
@@ -6,27 +6,33 @@ classdef PrideOfLion
         male
         female
         cubs
+        % call back function
+        fitnessFunc
+    end
+    
+    properties (Constant)
+        numOfCubs = 4
     end
     
     methods
-        function obj = PrideOfLion(male, female)
+        function obj = PrideOfLion(male, female, fitness)
             % Init the male and female of prode
             obj.male = male;
             obj.female = female;
+            obj.fitnessFunc = fitness;
+            obj.cubs = Lion.empty(0, obj.numOfCubs);
         end
         
         function obj = crossover(obj)
             % Let the male and female crossover and bron cubs
             % Before coressover, kill the previous generation first
-            obj.cubs = [];
             
             % Bron new cubs
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+            for i = 1 : obj.numOfCubs
+                crossPercent = rand;
+                newValue = obj.male.value * crossPercent + obj.female.value * (1 - crossPercent);
+                obj.cubs(i) = Lion(newValue, obj.fitnessFunc);
+            end
         end
     end
 end
